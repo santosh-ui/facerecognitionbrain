3a6a79ece8385839265146577f33183512dc3abf
santosh-ui <16r91a0547tkrec@gmail.com>
2020-07-29 09:26:06 +0530

    Added an image link input form

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+	return (
+		<div>
+			<p className='f3'>
+				{'This Magic Brain Will Detect Faces in your Image. Give it a try.'}
+			</p>
+			<div className='center'>
+			<div className='form center pa4 br3 shadow-5' >
+				<input className='f4 pa2 w-70 center' type='text' placeholder='input an image link' />
+				<button className='f4 w-30 grow link ph3 pv2 dib white bg-light-purple' >Detect</button>
+			</div>
+			</div>
+		</div>
+	);
+}
+
+export default ImageLinkForm;
