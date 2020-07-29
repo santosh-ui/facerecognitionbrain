db27a2ab396523765fa1d7737ea074d0180a3ffc
santosh-ui <16r91a0547tkrec@gmail.com>
2020-07-29 10:23:42 +0530

    Added styles with react particles js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 100,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+}
 
 function App() {
   return (
     <div className="App">
+    	<Particles className='particles'
+              params={particlesOptions}
+            />
     	<Navigation />
     	<Logo />
+    	<Rank />
     	<ImageLinkForm />
             {/*<FaceRecognition />*/}
     </div>
